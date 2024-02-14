aaff93034d17087c75b748a4b3359db54cd00713
akshayvakil <157507818+akshayvakil@users.noreply.github.com>
2024-02-14 10:45:56 +0530

    Max value in array and String Reverse code published

--- src/basicJavaPrograms/FindmaxValueInArray.java
+++ src/basicJavaPrograms/FindmaxValueInArray.java
@@ -4,12 +4,33 @@ public class FindmaxValueInArray {
 
 	public static void main(String[] args) {
 		int[] intarr = { 10, 2, 3, 30, 12 };
+		
+// Find Max Value in array		
 		int max = intarr[0];
 		for (int i = 0; i < intarr.length; i++) {
 			if (intarr[i] > max) {
 				max = intarr[i];
 			}
 		}
-		System.out.println(max);
+		System.out.println("Max Value is -->"+max);
+// Find Min Value in array
+		int minValue =intarr[0];
+		
+	for (int j=intarr.length-1;j>=0;j--)
+	{
+		if (intarr[j]<minValue)
+		{
+			minValue=intarr[j];
+		}
+		
 	}
+	System.out.println("Min Value is -->"+minValue);	
+
+		
+		
+		
+	}
+
+	
+	
 }
--- src/basicJavaPrograms/ReverseString.java
+++ src/basicJavaPrograms/ReverseString.java
@@ -7,22 +7,49 @@ public class ReverseString {
 		String str = "Selenium Testing";
 		String reversedstr = "";
 		char ch;
-//Using one character
+//Apporach1: Using iterating  characters
 		for (int i = 0; i < str.length(); i++) {
 			ch = str.charAt(i);
 			reversedstr = ch + reversedstr;
 
 		}
 		System.out.println(reversedstr);
-		
-		StringBuilder abc= new StringBuilder();
-		abc.append(reversedstr);
-		
+//Apporach2: Using StringBuilder.reverse Method
+
+		StringBuilder reverseusingStringBuilder = new StringBuilder(reversedstr);
+		// You can use appeand method as well
+		// reverseusingStringBuilder.append(reversedstr);
+
 		System.out.println("==Reverse again with the help of string builder==");
-		System.out.println(abc.reverse());
-		
+		System.out.println(reverseusingStringBuilder.reverse());
+
+//Apporach3: Using StringBuffer.reverse Method
+		System.out.println("==Reverse again with the help of string Buffer==");
+		StringBuffer stringbuff = new StringBuffer(reversedstr);
+		System.out.println(stringbuff);
+
+// Approach 4: Reverse all the characters using the Split() method.
+
+		String Name = "Selenium Testing";
+
+		String[] ReversedSplit = Name.split("");
+		System.out.println("==Reverse using split method");
+		for (int i = ReversedSplit.length - 1; i >= 0; i--) {
+			System.out.print(ReversedSplit[i]);
+
+		}
+// Approach 5: Reverse all the words using the Split() method.
+
+				String Sentence = "Selenium Testing";
+
+				String[] ReversedSplitSentence = Sentence.split(" ");
+				System.out.println("==Reverse using split method for words");
+				System.out.print("\n");
+				for (int i = ReversedSplitSentence.length - 1; i >= 0; i--) {
+					System.out.print(ReversedSplitSentence[i]+" ");
+
+				}
 
 	}
-	
 
 }
