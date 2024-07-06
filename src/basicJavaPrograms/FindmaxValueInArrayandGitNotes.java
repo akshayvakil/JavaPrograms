66573867b37f3e2cc9cc184056e4845daa4105e7
akshayvakil <157507818+akshayvakil@users.noreply.github.com>
2024-07-06 14:39:53 +0530

    Tesring commit and revert Revert "test revert"
    This reverts commit 8da2075f60bc25838cf55aa9fb29dc5bae6d76d6.

--- src/basicJavaPrograms/FindmaxValueInArrayandGitNotes.java
+++ src/basicJavaPrograms/FindmaxValueInArrayandGitNotes.java
@@ -4,7 +4,7 @@ package basicJavaPrograms;
 public class FindmaxValueInArrayandGitNotes {
 
 
-	/** test revet again s
+	/** test revet again
 	 *  Basic git commands https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html  
 	 * git add *
 	 * git commit -m "commit message"
